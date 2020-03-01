61706a9804df7c7d6cda3f0ae857d0fd41b32e67
rijimuge <david.russell04@gmail.com>
2020-03-01 17:06:51 -0600

    ActiveUserDeleteCommand added functionality

--- src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
+++ src/main/java/edu/uark/registerapp/commands/activeUsers/ActiveUserDeleteCommand.java
@@ -2,40 +2,33 @@ package edu.uark.registerapp.commands.activeUsers;
 
 import java.util.Optional;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import edu.uark.registerapp.commands.ResultCommandInterface;
+import edu.uark.registerapp.commands.VoidCommandInterface;
 import edu.uark.registerapp.commands.exceptions.UnauthorizedException;
 import edu.uark.registerapp.models.entities.ActiveUserEntity;
 import edu.uark.registerapp.models.repositories.ActiveUserRepository;
 
 @Service
-public class ActiveUserDeleteCommand implements ResultCommandInterface<ActiveUserEntity> {
+public class ActiveUserDeleteCommand implements VoidCommandInterface {
 	@Override
-	public ActiveUserEntity execute() {
+	public void execute() {
 		final Optional<ActiveUserEntity> activeUserEntity =
-			this.activeUserRepository.findBySessionKey(this.sessionKey);
-
-		if (!activeUserEntity.isPresent()) {
-			throw new UnauthorizedException();
-		}
-
-		return activeUserEntity.get();
+			this.activeUserRepository.findBySessionKey(this.request.getSession().getId());
+			activeUserRepository.delete(activeUserEntity.get());
 	}
 
 	// Properties
-	private String sessionKey;
-
-	public String getSessionKey() {
-		return this.sessionKey;
-	}
-
-	public ValidateActiveUserCommand setSessionKey(final String sessionKey) {
-		this.sessionKey = sessionKey;
-		return this;
-	}
+	private HttpServletRequest request;
 
 	@Autowired
 	private ActiveUserRepository activeUserRepository;
+
+	public ActiveUserDeleteCommand setRequest(HttpServletRequest request) {
+		this.request = request;
+		return this;
+	}
 }
