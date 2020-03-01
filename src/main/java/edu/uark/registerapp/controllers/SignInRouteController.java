3395923009b7542fd0c25b0ce46e5b8fef12c96b
rijimuge <david.russell04@gmail.com>
2020-03-01 12:27:47 -0600

    some changes

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -4,16 +4,16 @@ import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
 
-import org.springframework.http.MediaType;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.EmployeeSignIn;
 import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
 @Controller
@@ -24,12 +24,8 @@ public class SignInRouteController extends BaseRouteController {
 	@RequestMapping(method = RequestMethod.GET)
 	public ModelAndView serveSignInPage(
 		@RequestParam final Map<String, String> queryParameters,
-		// TODO: Define an object that will represent the sign in request and add it as a parameter here
 		HttpServletRequest request
 	) {
-		// TODO: Use the credentials provided in the request body
-		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
-		//  to sign in the user
 
 		if (employeeRepository.count() > 0) {
 				return new ModelAndView(ViewNames.SIGN_IN.getViewName());
@@ -44,20 +40,17 @@ public class SignInRouteController extends BaseRouteController {
 	 consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
 	public ModelAndView performSignIn(
 		@RequestParam final Map<String, String> queryParameters,
+		EmployeeSignIn employeeSignIn,
 		// TODO: Define an object that will represent the sign in request and add it as a parameter here
 		HttpServletRequest request
 	) {
+		return new ModelAndView(
+				REDIRECT_PREPEND.concat(
+					ViewNames.EMPLOYEE_DETAIL.getRoute()));
 		// TODO: Use the credentials provided in the request body
 		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
 		//  to sign in the user
 
-		if (employeeRepository.count() > 0) {
-				return new ModelAndView(ViewNames.SIGN_IN.getViewName());
-		} else {
-			return new ModelAndView(
-				REDIRECT_PREPEND.concat(
-					ViewNames.EMPLOYEE_DETAIL.getRoute()));
-		}
 	}
 
 	/*@RequestMapping(value = "/signIn", method = RequestMethod.GET)
