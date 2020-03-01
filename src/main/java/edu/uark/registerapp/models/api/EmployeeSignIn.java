72d3b411ea7acc60121123870632d2d6b5fa6bcd
rijimuge <david.russell04@gmail.com>
2020-03-01 09:32:27 -0600

    EmployeeSignIn changes

--- src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
+++ src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
@@ -6,24 +6,37 @@ import java.util.UUID;
 
 import org.apache.commons.lang3.StringUtils;
 
-import edu.uark.registerapp.models.entities.ProductEntity;
+import edu.uark.registerapp.models.entities.EmployeeEntity;
+import edu.uark.registerapp.commands.employees.helpers.EmployeeHelper;
 
 public class EmployeeSignIn extends ApiResponse {
   private String employeeId;
   public String getEmployeeID() {
-      return this.employeeId;
+    return this.employeeId;
   }
   public EmployeeSignIn setEmployeeId(final String employeeId) {
-      this.employeeId = employeeId;
-      return this;
+    this.employeeId = employeeId;
+    return this;
   }
-  private String employeeId;
-  public String getEmployeeID() {
-      return this.employeeId;
+  private String password;
+  public String getPassword() {
+    return this.password;
   }
-  public EmployeeSignIn setEmployeeId(final String employeeId) {
-      this.employeeId = employeeId;
-      return this;
+  public EmployeeSignIn setPassword(final String password) {
+    this.password = password;
+    return this;
+  }
+
+  public EmployeeSignIn() {
+      super();
+      this.employeeId = "";
+      this.password = "";
+  }
+
+  public EmployeeSignIn(final EmployeeEntity employeeEntity) {
+      super(false);
+      this.employeeId = EmployeeHelper.padEmployeeId(employeeEntity.getEmployeeId());
+      this.password = new String(employeeEntity.getPassword());
   }
 
 }
