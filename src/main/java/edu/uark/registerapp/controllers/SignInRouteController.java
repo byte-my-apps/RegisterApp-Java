b6c4f72986a6f64a834faea944b688df2447f75f
rijimuge <david.russell04@gmail.com>
2020-03-01 11:19:24 -0600

    redirects to EmployeeDetail if employees in table, otherwise, serves signIn page

--- src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -1,14 +1,20 @@
 package edu.uark.registerapp.controllers;
 
+import java.util.Map;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.http.MediaType;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.repositories.EmployeeRepository;
 
 @Controller
 @RequestMapping(value = "/")
@@ -16,17 +22,52 @@ public class SignInRouteController extends BaseRouteController {
 	// TODO: Route for initial page load
 
 	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView performSignIn(
+	public ModelAndView serveSignInPage(
+		@RequestParam final Map<String, String> queryParameters,
 		// TODO: Define an object that will represent the sign in request and add it as a parameter here
 		HttpServletRequest request
 	) {
-
 		// TODO: Use the credentials provided in the request body
 		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
 		//  to sign in the user
 
-		return new ModelAndView(
-			REDIRECT_PREPEND.concat(
-				ViewNames.MAIN_MENU.getRoute()));
+		if (employeeRepository.count() > 0) {
+				return new ModelAndView(ViewNames.SIGN_IN.getViewName());
+		} else {
+			return new ModelAndView(
+				REDIRECT_PREPEND.concat(
+					ViewNames.EMPLOYEE_DETAIL.getRoute()));
+		}
 	}
+
+	@RequestMapping(method = RequestMethod.POST,
+	 consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	public ModelAndView performSignIn(
+		@RequestParam final Map<String, String> queryParameters,
+		// TODO: Define an object that will represent the sign in request and add it as a parameter here
+		HttpServletRequest request
+	) {
+		// TODO: Use the credentials provided in the request body
+		//  and the "id" property of the (HttpServletRequest)request.getSession() variable
+		//  to sign in the user
+
+		if (employeeRepository.count() > 0) {
+				return new ModelAndView(ViewNames.SIGN_IN.getViewName());
+		} else {
+			return new ModelAndView(
+				REDIRECT_PREPEND.concat(
+					ViewNames.EMPLOYEE_DETAIL.getRoute()));
+		}
+	}
+
+	/*@RequestMapping(value = "/signIn", method = RequestMethod.GET)
+	public ModelAndView startSignIn(
+		@RequestParam final Map<String, String> queryParameters,
+		HttpServletRequest request
+	){
+			} */
+
+
+	@Autowired
+	private EmployeeRepository employeeRepository;
 }
--- src/main/resources/static/scripts/signIn.js
+++ src/main/resources/static/scripts/signIn.js
@@ -3,6 +3,33 @@ document.addEventListener("DOMContentLoaded", function(event) {
 });
 
 function validateForm() {
-	// TODO: Validate the user input
-	return true;
+	const employeeId = getEmployeeId();
+	if ((employeeId == null) || (isNaN(employeeId))) {
+		displayError("Please provide a valid Employee ID.");
+		const firstNameElement = getEmployeeFirstNameElement();
+		firstNameElement.focus();
+		firstNameElement.select();
+		return false;
+	}
+
+	const password = getEmployeePassword();
+		if (password == null || password.trim() === "" ) {
+		displayError("Please provide a valid password.");
+		const passwordElement = getPasswordElement();
+		passwordElement.focus();
+		passwordElement.select();
+		return false;
+	}
+}
+function getEmployeeIdElement() {
+	return document.getElementById("employeeId");
+}
+function getEmployeeId() {
+	return getEmployeeIdElement().value;
+}
+function getEmployeePasswordElement() {
+	return document.getElementById("password");
+}
+function getEmployeePassword() {
+	return getEmployeePasswordElement().value;
 }
